394ecb0f57564166822b91d22bb5b7559585ed55
Tim DeGraw <tdegraw@gmail.com>
2017-08-14 21:02:06 -0700

    initial commit

---
+++ Desktop/Homework Folder/Week 4 HW/week-4-game/assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $("#btn1").click(function(){
+        $("ul").append("val = btn1");
+    });
+
+    $("#btn2").click(function(){
+        $("ul").append("val = btn2");
+    });
+
+    $("#btn3").click(function(){
+        $("ul").append("val = btn3");
+    });
+
+    $("#btn4").click(function(){
+        $("ul").append("val = btn4");
+    });        
+});
+
